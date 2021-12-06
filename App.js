fa552417f997c4aef9471423b9584df5f1340ea7
LisaJeong <LisaJeong@users.noreply.github.com>
2021-12-06 04:12:13 -0500

    signin pg

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Button, Pressable } from 'react-native';
 
-export default function App() {
+export default function App(props) {
+  const { onPress, title = 'UNI Login' } = props;
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}> LionGuard</Text>
+      <Pressable style={styles.btn} onPress={onPress}>
+        <Text style={styles.txt}>{title}</Text>
+      </Pressable>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +18,31 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'rgb(185, 217, 235)',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    color: '#000d74',
+    fontSize: 50,
+    fontWeight: 'bold',
+  },
+  btn: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: '#000d74',
+    marginTop: 30,
+  },
+
+  txt: {
+    fontSize: 20,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: '#ffffff',
+  }
 });
